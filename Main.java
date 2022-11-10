92dd2071de63ca4ffb138ba42ff2231ed22e7243
k.yoshida <k.yoshida.sys22@morijyobi.ac.jp>
2022-11-10 10:48:34 +0900

    Main.javaに追記

--- Main.java
+++ Main.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
 public class Main{
 	public static void main(String[] args){
-		double i = 3.24;
-		double s = 78.0;
-		double bmi = s / i;
+		Scanner s = new Scanner(System.in);
+		
+		double height = (double)s.nextInt();
+		double i = (height / 100) * (height / 100);
+		double weight = (double)s.nextInt();
+		double bmi = weight / i;
 		double r = (Math.round(bmi * 100)) /100;
 		System.out.println(r);
+		
+                double height2 = (double)s.nextInt();
+                double u = (height2 / 100) * (height2 / 100);
+                double weight2 = (double)s.nextInt();
+                double bmi2 = weight2 / u;
+                double r2 = (Math.round(bmi2 * 100)) /100;
+                System.out.println(r2);
+
 	}
 }
